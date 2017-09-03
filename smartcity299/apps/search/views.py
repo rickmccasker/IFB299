252bd860bbaffe61a6fa1f40305264c7c8747107
Rick Mccasker <rickmccasker@gmail.com>
2017-09-03 14:20:02 +1000

    Revert "Revert "DB functionality and can display text in browser. Needs formatting.""
    This reverts commit f1aaf43e1c57fa1bb6bcdd38f84736345e150f9f.

--- smartcity299/apps/search/views.py
+++ smartcity299/apps/search/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from django.template import loader
 from django.template import Context
 
+from ..search.models import Services
+
 def search(request):
 	sQuery = request.GET['sQuery']
+	resultSet = Services.objects.filter(name__icontains=sQuery)
 	t = loader.get_template('results.html')
-	c = Context({ 'sQuery' : sQuery})
+	c = Context({ 'resultSet' : resultSet})
+
 	return HttpResponse(t.render(c))
