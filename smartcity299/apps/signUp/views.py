affd210563eea6704de6b1a8e23063d416e0c7b0
Rick Mccasker <rickmccasker@gmail.com>
2017-09-11 17:44:14 +1000

    Additional tables & inputs for regi

--- smartcity299/apps/signUp/views.py
+++ smartcity299/apps/signUp/views.py
@@ -13,9 +13,14 @@ def drawSignUp(request):
 def createUser(request):
 	user = User.objects.create_user(username = request.POST['username'], 
 									email = request.POST['email'],
-									password = request.POST['password'])
+									password = request.POST['password'], 
+									first_name = request.POST['firstname'],
+									last_name = request.POST['lastname'])
 	#user.last_name = "x" insert special values
-	userExtend = UserProfile(user_id = user.id, address="EXAMPLE ADDRESS") #add input for this
+	userExtend = UserProfile(user_id = user.id, 
+							address=request.POST["address"],
+							user_type=request.POST["usertype"],
+							phone_number = request.POST["phone"]) #add input for this
 	userExtend.save()
 	user.save()
 	#print(user.userprofile.address) This is how you call it
