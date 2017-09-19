14785d6113435d75cdc1aab7521168d6d4c5476b
Rick Mccasker <rickmccasker@gmail.com>
2017-09-19 17:21:45 +1000

    Admin account creates properly with redirect and meaningful response to user.

--- smartcity299/apps/adminControls/urls.py
+++ smartcity299/apps/adminControls/urls.py
@@ -5,6 +5,7 @@ from . import views as admin_views
 urlpatterns = [
 	url(r'^$', admin_views.drawControlPage),
 	url(r'add_admin/$', admin_views.drawAddAdmin),
+	url(r'add_admin/attempt_addadmin/$', admin_views.addAdmin), 
 	url(r'add_page/$', admin_views.drawSelectModelPage),
 	url(r'(\w+)/$', admin_views.drawAddModelPage, name="addModels")
 ]
