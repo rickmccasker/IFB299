5c947a386fe139f68f292840f243e53144827efb
Rick Mccasker <rickmccasker@gmail.com>
2017-09-22 07:20:03 +1000

    Prep for finalising sprint. Readjusted some styles and removed dead attributes/tags.

--- smartcity299/apps/login/views.py
+++ smartcity299/apps/login/views.py
@@ -5,22 +5,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
-#Render login.html template
+#Draw
 def drawLogin(request):
+	"""
+	If the user is authenticated redirect to the search page
+	Else allow access to the login and render
+	"""
 	if(request.user.is_authenticated):
 		return redirect('/search/')
 	else:
 		return render(request, 'login.html')
 	
 
-#Retrieve post and check if user exists in database. If so direct to home page, if not
-#Loop back to login page with new prompts
+#Logic
 def auth_user(request):
+	"""
+	Check if the user's input matches a row in the db
+	If it does redirect back to home, which should redirect to the search page
+	If it does not, send user back to login with a meaningful error message
+	"""
 	user = authenticate(username=request.POST['username'], password=request.POST['password'])
 	if user is not None:
 		login(request, user)
 		return redirect('/')
 	else:
-		#"This user doesnt exist, change page accordingly, Possible with redirect?"
 		messages.add_message(request, messages.ERROR, 'Incorrect username or password')
 		return redirect('/login/')
