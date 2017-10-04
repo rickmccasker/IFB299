14223651c808a142ab8f0c102bb8be3ce39c1eb2
Rick Mccasker <rickmccasker@gmail.com>
2017-10-04 15:05:46 +1000

    Adjusted git ignore. Added basic map with markers. Info supplied by googlemaps.

---
+++ smartcity299/static/javascript/maps.js
@@ -0,0 +1,42 @@
+var map;
+var infowindow;
+
+function initMap(latitude, longitude){
+    var location = { lat: latitude, lng: longitude };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: location,
+        radius: 500,
+        type: ['park']
+    }, callback);
+
+    //for loop based on given db array
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
