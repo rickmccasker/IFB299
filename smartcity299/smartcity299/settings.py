34f67d572d45895e19c708ab52943814efea9e36
Rick Mccasker <rickmccasker@gmail.com>
2017-10-23 11:37:24 +1000

    Hosting stuff

--- smartcity299/smartcity299/settings.py
+++ smartcity299/smartcity299/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
